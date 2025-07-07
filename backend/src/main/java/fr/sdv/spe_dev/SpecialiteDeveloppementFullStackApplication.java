05abd3a99710cf49717ee55e60189ab4dcd1b1a8
Zh00rg <theo.charron@cyna-it.fr>
2025-07-07 09:57:35 +0200

    Project Skeleton

---
+++ backend/src/main/java/fr/sdv/spe_dev/SpecialiteDeveloppementFullStackApplication.java
@@ -0,0 +1,13 @@
+package fr.sdv.spe_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialiteDeveloppementFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialiteDeveloppementFullStackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fr/sdv/spe_dev/SpecialiteDeveloppementFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sdv.spe_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecialiteDeveloppementFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
